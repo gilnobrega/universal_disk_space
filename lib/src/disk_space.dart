ff840802e5a1a7298eabe42906b0b3fa90271211
Joaquim Guimarães <jgilfnuk@gmail.com>
2021-04-18 21:26:29 +0100

    getDisk function returns disk info for a given path

--- lib/src/disk_space.dart
+++ lib/src/disk_space.dart
@@ -38,14 +38,39 @@ class DiskSpace {
           disks.add(new Disk(
               devicePath, mountPath, totalSize, usedSpace, availableSpace));
         }
+        
+        //orders from longer mountpath to shorter mountpath, very important as getDisk would break otherise
+        disks.sort((disk2, disk1) => disk1._mountPath.length.compareTo(disk2._mountPath.length));
       }
       //throws exception if df doesnt exist
       else
-        throw new BinNotFoundException(
+        throw new NotFoundException(
             "Could not locate df binary in " + dfLocation);
     }
     //Windows code
     else if (io.Platform.isWindows) {}
+  }
+
+  Disk getDisk(String path) //Gets info of disk of given path
+  {
+    io.FileSystemEntity entity;
+
+    if (io.File(path).existsSync())
+      entity = io.File(path);
+    else if (io.Directory(path).existsSync())
+      entity = io.Directory(path);
+    else
+      throw new NotFoundException(
+          "Could not locate the following file or directory: " + path);
+
+    for (Disk disk in disks) {
+      if (entity.absolute.path.startsWith(disk.mountPath) ||
+          entity.absolute.path.startsWith(disk._devicePath)) return disk;
+    }
+
+      throw new NotFoundException(
+          "Unable to get information about disk which contains " + path);
+
   }
 }
 
--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -1,15 +1,27 @@
 //https://www.woolha.com/tutorials/dart-creating-custom-exception-class
 
-class BinNotFoundException implements Exception
-{
-    String _message;
-  
-    BinNotFoundException([String message = "Failed to locate binaries!"]) {
-      this._message = message;
-    }
-  
-    @override
-    String toString() {
-      return _message;
-    }
+class NotFoundException implements Exception {
+  String _message;
+
+  NotFoundException([String message = "Failed to locate path."]) {
+    this._message = message;
+  }
+
+  @override
+  String toString() {
+    return _message;
+  }
+}
+
+class DiskInfoError implements Exception {
+  String _message;
+
+  DiskInfoError([String message = "Failed to get disk info."]) {
+    this._message = message;
+  }
+
+  @override
+  String toString() {
+    return _message;
+  }
 }
